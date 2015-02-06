796a6af29a250b644475283c9b52f04a4789f3fa
Francis De Brabandere <francisdb@gmail.com>
2015-02-06 11:35:13 +0100

    fix correct sending of json body

--- monitor.sh
+++ monitor.sh
@@ -9,35 +9,22 @@ uname -a
 RESOURCE="waylaypi"
 
 function fetchStatsAndStore {
-    echo "Memory Space Details:"
     TOTAL=`free -t -m | grep Total`;
     TOTAL_MEM=`echo $TOTAL | awk '{ print $2}'`
     USED_MEM=`echo $TOTAL | awk '{ print $3}'`
     FREE_MEM=`echo $TOTAL | awk '{ print $4}'`
-    echo "Total memory=$TOTAL_MEM MB"
-    echo "Used memory=$USED_MEM MB"
-    echo "Free memory=$FREE_MEM MB"
 
-    echo "Swap memory Details:"
     SWAP=`free -t -m | grep Swap`
     TOTAL_SWAP=`echo $SWAP | awk '{ print $2}'`
     USED_SWAP=`echo $SWAP | awk '{ print $3}'`
     FREE_SWAP=`echo $SWAP | awk '{ print $4}'`
-    echo "Total swap=$TOTAL_SWAP MB"
-    echo "Used swap=$USED_SWAP MB"
-    echo "Free swap=$FREE_SWAP MB"
 
-    echo "Check disk Space:"
     DISK=`df --block-size=1 /  | grep dev`
     TOTAL_DISK=`echo $DISK | awk '{ print $2}'`
     USED_DISK=`echo $DISK | awk '{ print $3}'`
     FREE_DISK=`echo $DISK | awk '{ print $4}'`
-    echo "Total disk=$TOTAL_DISK B"
-    echo "Used disk=$USED_DISK B"
-    echo "Free disk=$FREE_DISK B"
 
     CPU_USAGE=`top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`
-    echo "CPU usage=$CPU_USAGE %"
 
     JSON_BODY='{
         "domain" : "'$WAYLAY_DOMAIN'",
@@ -52,7 +39,9 @@ function fetchStatsAndStore {
         "usedDisk" : '$USED_DISK',
         "freeDisk" : '$FREE_DISK' }'
 
-    curl --user $WAYLAY_KEY:$WAYLAY_SECRET -H "Content-Type:application/json" -X POST -d ${JSON_BODY} https://data.waylay.io/resources/${RESOURCE} || true
+    echo ${JSON_BODY}
+
+    curl --user $WAYLAY_KEY:$WAYLAY_SECRET -H "Content-Type:application/json" -X POST -d "$JSON_BODY" https://data.waylay.io/resources/${RESOURCE} || true
     # the || true makes sure the command does not exit on thing like: Couldn't resolve host 'data.waylay.io'
 }
 
