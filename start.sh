398bf46278e5a09ad90308c1f9d904d62141ac54
Francis De Brabandere <francisdb@gmail.com>
2015-03-09 21:47:40 +0100

    added avahi mdns

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+
+# Restart service
+/etc/init.d/avahi-daemon restart
+
 udevd &
 #modprobe bcm2708_wdog
 modprobe i2c-bcm2708
